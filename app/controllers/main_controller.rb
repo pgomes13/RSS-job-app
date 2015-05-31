8ab67c0404f7d82be81fefede8b1befb3f110ef7
pritgomes <Pasci@Pascoals-MacBook-Air.local>
2015-05-31 20:45:38 +0930

    RSS job app completed

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,5 @@
+class MainController < ApplicationController
+def welcome
+     @num_sources = Job.count
+   end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoolRSSJobApp::Application.config.secret_token = '2845494b615a0a157766d3ea5c92ca1201be8a172ddb62139498d8c9565a8b01f96b804d535b09d455d80b72e903246eb14c13da348900b650c5bb4c15d4e6bd'
